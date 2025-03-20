8a166597197f6925d75ad364118b416cf87a60ed
Steffan Troxel <sjtroxel@alumni.iu.edu>
2025-03-20 15:05:40 -0500

    notes & two exercises from Th. Mar20 class

---
+++ task-manager-app-main/src/app/task/task.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  imports: [],
+  standalone: true,
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
+})
+export class TaskComponent {
+
+}
